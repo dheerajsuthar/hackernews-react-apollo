9db4c2cccaf4d2259fdcdd76574b988497457f01
Dheeraj Suthar <dheersuthar@gmail.com>
2018-12-21 02:28:44 +0530

    Bootstrapped header.

--- src/Header.js
+++ src/Header.js
@@ -2,27 +2,32 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router';
 import { AUTH_TOKEN } from './constants';
+import { Nav, Navbar, NavItem } from 'react-bootstrap';
 
 class Header extends Component {
     render() {
         const token = localStorage.getItem(AUTH_TOKEN);
         console.log(token);
-        
+
         return (
-            <div>
-                <div>LinkShare</div>
-                <Link to="/">home</Link>
-                <div>|</div>
-                {token ? <Link to="/submit">submit</Link> : <Link to="/login">login</Link>}
-                {token &&
-                    (<div>
-                        <div>|</div>
-                        <div
+            <Navbar>
+                <Navbar.Header>LinkShare</Navbar.Header>
+                <Nav>
+                    <NavItem componentClass={Link} href="/" to="/">
+                        Home
+                    </NavItem>
+                    {token ? <NavItem componentClass={Link} href="/submit" to="/submit">
+                        Submit New Link
+                    </NavItem> : <NavItem componentClass={Link} href="/login" to="/login">
+                            Login
+                        </NavItem>}
+                    {token &&
+                        <NavItem
                             onClick={() => { localStorage.removeItem(AUTH_TOKEN); this.props.history.push('/') }}
-                        >signout</div>
-                    </div>
-                    )}
-            </div>
+                        >Logout</NavItem>
+                    }
+                </Nav>
+            </Navbar>
         );
     }
 }
