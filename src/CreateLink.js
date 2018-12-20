5510729f10033a35e78a55ae1bb8083acceb00b2
Dheeraj Suthar <dheersuthar@gmail.com>
2018-12-21 02:45:19 +0530

    Bootstrapped CreateLink.

--- src/CreateLink.js
+++ src/CreateLink.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import { FEED_QUERY } from './LinkList';
+import { FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap';
+
 
 const MUTATION = gql`
     mutation PostMutation($url: String!, $description: String!){
@@ -31,35 +33,41 @@ class CreateLink extends Component {
         const url = this.state.url;
         const description = this.state.description;
         return (
-            <div>
-                <input
-                    value={url}
-                    onChange={e => this.setState({ url: e.target.value })}
-                    placeholder="URL"
-                />
-                <input
-                    value={description}
-                    onChange={e => this.setState({ description: e.target.value })}
-                    placeholder="Description"
-                />
-
+            <form>
+                <FormGroup>
+                    <ControlLabel>URL</ControlLabel>
+                    <FormControl
+                        type="text"
+                        value={url}
+                        onChange={e => this.setState({ url: e.target.value })}
+                        placeholder="www.imdb.com"
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <ControlLabel>Description</ControlLabel>
+                    <FormControl
+                        value={description}
+                        onChange={e => this.setState({ description: e.target.value })}
+                        placeholder="Random ratings."
+                    />
+                </FormGroup>
                 <Mutation
                     mutation={MUTATION}
                     variables={{ url, description }}
                     onCompleted={() => this.props.history.push('/')}
-                    update={(store, res)=>{
+                    update={(store, res) => {
                         const link = res.data.createLink;
                         console.log(link);
-                        const data = store.readQuery({query: FEED_QUERY});
+                        const data = store.readQuery({ query: FEED_QUERY });
                         data.link.push(link);
-                        store.writeQuery({query: FEED_QUERY, data: data});
+                        store.writeQuery({ query: FEED_QUERY, data: data });
                     }}
                 >
                     {
-                        postMutation => <button onClick={postMutation}>Submit</button>
+                        postMutation => <Button onClick={postMutation}>Submit</Button>
                     }
                 </Mutation>
-            </div>
+            </form>
         );
     }
 }
